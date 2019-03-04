ce2989cc99cb0038a84e14a462b3855d7de2d9c3
Irina <murlucien@gmail.com>
2019-03-04 18:44:07 +0300

    lines

--- file.py
+++ file.py
@@ -1,8 +1,11 @@
-with open('referat.txt', 'r', encoding='utf-8') as f:
-    fil = f.read()
-    print(len(fil))
-    words = fil.split()
-    print(len(words))
-    
-with open('referat2.txt', 'w', encoding='utf-8') as f:
-    fil = f.write(fil.replace('.', '!'))
+with open('referat.txt', 'r', encoding='utf-8') as f, open('referat2.txt', 'w', encoding='utf-8') as out:
+    le = 0
+    words = 0
+    for lin in f:
+        l = lin.rstrip()
+        words += len(l.split())
+        le += len(l)
+        new = lin.replace('.', '!')
+        n = out.write(new)
+    print(le)
+    print(words)  
